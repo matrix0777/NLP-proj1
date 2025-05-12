e24d9489c8b38877aa592485a378ce9982b44f4f
Dhananjay_07 <mandlapured@gmail.com>
2025-05-13 03:26:35 +0530

    Pridiction pipeline addes

--- demo.py
+++ demo.py
@@ -1,7 +1,5 @@
-from hate.logger import logging
-from hate.exception import CustomException
-import sys
-from hate.configuration.gcloud_syncer import GCloudSync
+from hate.pipeline.train_pipeline import TrainPipeline
 
-obj = GCloudSync()
-obj.sync_folder_from_gcloud("nlp-proj2025","dataset.zip","dataset.zip")
+
+obj = TrainPipeline()
+obj.run_pipeline()
